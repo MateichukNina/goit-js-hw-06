d167ffacce46af32df1e5ca560a644415b6d5898
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2023-05-25 12:56:22 +0300

    Add files via upload

---
+++ task-06.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector("#validation-input");
+
+textInput.addEventListener("blur", (event) => {
+    const inputLength = parseInt(event.currentTarget.dataset.length);
+    textInput.classList.remove("invalid", "valid");
+
+
+    if(textInput.value.length !== inputLength){
+        textInput.classList.add("invalid");
+    }
+    else  {
+    textInput.classList.add("valid");};
+});
